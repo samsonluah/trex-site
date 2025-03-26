d0b66ef085e60a20c87208744855cec574ac7013
Jun Yang <yangjunluah@gmail.com>
2025-03-26 12:25:01 +0800

    Added product images for ORIGIN TSHIRT

--- src/services/ProductData.ts
+++ src/services/ProductData.ts
@@ -24,7 +24,7 @@ export const products: Product[] = [
     formattedPrice: 'S$29.99',
     description: 'Lightweight cotton T-shirt in acid-washed black, with our ORIGIN logo printed on the back. NOTE: The pictured sample is missing the logo on the front, which is shown on Image 5.',
     longDescription: 'Our signature ORIGIN T-shirt is made from lightweight cotton for maximum comfort for casual wear. The acid-washed black fabric gives it a vintage look, while the ORIGIN logo printed on the back provides a clean, minimalist design, a call back to our origins 1 year ago. Perfect for gym sessions, or just looking sick all around. NOTE: The pictured sample is missing the logo on the front, which is shown on Image 5.',
-    images: ['/placeholder.svg', '/placeholder.svg', '/placeholder.svg'],
+    images: ['/DSC09579.jpg','/DSC09583.jpg'],
     slug: 'tshirt',
     sizes: ['S', 'M', 'L', 'XL', 'XXL'],
     inStock: true,
